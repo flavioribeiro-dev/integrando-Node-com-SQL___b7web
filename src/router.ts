b55d8924a7c7b9a5f61078b4b5c63033d978d906
fribeiro <flavioxconsult@gmail.com>
2026-08-08 21:37:04 -0300

    configurada rota de inclusão de registros no Banco de Dados

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,22 @@
 import { Router } from "express";
+import prisma from "./libs/prisma.js";
 
 const router = Router();
+
+router.post('/usuarios', (req, res) => {
+    const user = prisma.user.create({
+        data: {
+            nome: "Henrique",
+            sobrenome: "Ribeiro",
+        }
+    })
+    return res.json({ user })
+})
+
+router.get('/usuarios', (req, res) => {
+    const users = prisma.user.findMany();
+    res.json({ users });
+})
 
 router.get('/', (req, res) => {
     res.send('página inicial')
