3c9125edc4deb7b9bf520a237b27d676694d83e6
fribeiro <flavioxconsult@gmail.com>
2026-08-13 00:56:35 -0300

    criando vários registros

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUser } from "./services/user.js";
+import { createUser, createUsers } from "./services/user.js";
+import { count } from "node:console";
 
 const router = Router();
 
-router.post('/usuarios', async (req, res) => {
+router.post('/user', async (req, res) => {
     // Validar os dados de entrada...
 
     const user = await createUser({ nome: 'Marcio', sobrenome: 'da Silva F', email: 'marcio@email.com' });
@@ -12,6 +13,16 @@ router.post('/usuarios', async (req, res) => {
     }
     return res.status(500).json({ error: 'email já cadastrado' });
 })
+
+router.post('/users', async (req, res) => {
+    const result = await createUsers([
+        { nome: 'Marilene', sobrenome: 'Silva', email: 'lene@yahoo.com' },
+        { nome: 'Rafaela', sobrenome: 'Silva', email: 'amoreca@yahoo.com' },
+        { nome: 'Coringa', sobrenome: 'Gomes', email: 'coring@yahoo.com' },
+    ]);
+    res.json({ result });
+})
+
 
 router.get('/', (req, res) => {
     res.send('página inicial')
